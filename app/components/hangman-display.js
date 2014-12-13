102b2a8bd49aab14eac600f3a4f4b48553b14e1b
Gary Wilson <github.ghw@codeprime.com>
2014-12-12 18:31:42 -0800

    css animation updates

--- app/components/hangman-display.js
+++ app/components/hangman-display.js
@@ -4,9 +4,27 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['hangman-display'],
 
-  images: ['head', 'body', 'right-arm', 'left-arm', 'right-leg', 'left-leg'],
+  head: function() {
+    return this.get('missedCount') > 0;
+  }.property('missedCount'),
 
-  bodyParts: function() {
-    return this.get('images').slice(0, this.get('missedCount'));
-  }.property('images', 'missedCount')
+  body: function() {
+    return this.get('missedCount') > 1;
+  }.property('missedCount'),
+
+  rightArm: function() {
+    return this.get('missedCount') > 2;
+  }.property('missedCount'),
+
+  leftArm: function() {
+    return this.get('missedCount') > 3;
+  }.property('missedCount'),
+
+  rightLeg: function() {
+    return this.get('missedCount') > 4;
+  }.property('missedCount'),
+
+  leftLeg: function() {
+    return this.get('missedCount') > 5;
+  }.property('missedCount')
 });
