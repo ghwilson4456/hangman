bb706e09b3d06db6a50eb6a5a37f5c714aab457e
Gary Wilson <Gary.Wilson19@T-Mobile.com>
2015-08-01 22:04:49 -0700

    category selection with random word, and code refinement

--- app/game/route.js
+++ app/game/route.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return Ember.RSVP.hash({
-        categories: this.store.findAll('category'),
-        celebrities: this.store.findAll('celebrity'),
-        movies: this.store.findAll('movie')
-    });
+  model: function(params) {
+    return this.store.findAll(params.category_id);
+  },
+
+  setupController: function(controller, model) {
+    var len    = model.get('length');
+    var record = model.objectAt(Math.floor(Math.random() * len));
+    var word   =record.get('name').toUpperCase().split('');
+    controller.set('word', word);
+    controller.set('guessed', []);
   }
 });
