979f855df2078c2f74d63400159a7021ab26aabc
Gary Wilson <Gary.Wilson19@T-Mobile.com>
2015-10-21 21:51:39 -0400

    Revised for Ember 2.1 and http-mock

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,5 @@
-import DS from "ember-data";
+import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api',
-  suffix: '.json',
-  pathForType(type) {
-    return this._super(type) + this.get('suffix');
-  }
+  namespace: 'api'
 });
