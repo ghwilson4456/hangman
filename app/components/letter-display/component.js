c88147cec80e8eb89648d035f8235f157fca2b1a
Gary Wilson <Gary.Wilson19@T-Mobile.com>
2016-01-22 11:02:43 -0800

    improvements

--- app/components/letter-display/component.js
+++ app/components/letter-display/component.js
@@ -1,6 +1,12 @@
-import Ember from 'ember';
+/**
+ * Selectively display letters from a word (or words).
+ * @param {number} class
+ * @param {number} word
+ * @param {string} letters
+ * @param {string} blank
+ */
 
-const get = Ember.get;
+import Ember from 'ember';
 
 const {
   Component,
@@ -12,26 +18,27 @@ export default Component.extend({
   tagName: 'section',
 
   displayLetters: computed('word', 'letters', 'blank', function() {
-    let letters = (get(this, 'letters')) ? get(this, 'letters') : [];
+    const letters = (get(this, 'letters')) ? get(this, 'letters') : [];
+    const className = get(this, 'class');
 
     return get(this, 'word').map(item => {
-      let code = item.toUpperCase().charCodeAt(0);
+      const code = item.toUpperCase().charCodeAt(0);
       let data = {
         char: item.toUpperCase(),
         alpha: (code >= 65 && code <= 90),
-        classes: (code >= 65 && code <= 90) ? 'c-LetterDisplay__char c-LetterDisplay__char--alpha' : 'c-LetterDisplay__char c-LetterDisplay__char--non-alpha'
+        classes: (code >= 65 && code <= 90) ? `${className}__char ${className}__char--alpha` : `${className}__char ${className}__char--non-alpha`
       };
 
       if (code === 32) {
         data.char = '&nbsp;';
-        data.classes = 'c-LetterDisplay__char c-LetterDisplay__char--space';
+        data.classes = `${className}__char ${className}__char--space`;
       } else {
         data.char = item;
       }
 
       if (letters.indexOf(item) === -1 && data.alpha === true) {
         data.char = get(this, 'blank') || '&nbsp;';
-        data.classes = 'c-LetterDisplay__char c-LetterDisplay__char--blank';
+        data.classes = `${className}__char ${className}__char--blank`;
       }
 
       return data;
--- app/components/sprite-viewer/component.js
+++ app/components/sprite-viewer/component.js
@@ -1,3 +1,15 @@
+/**
+ * Display a single sprite from a spritesheet by index.
+ * @param {number} index
+ * @param {number} length
+ * @param {string} sprite
+ * @param {string} maxScale
+ * @param {number} width
+ * @param {number} height
+ * @param {number} columns
+ * @param {boolean} disableInlineStyles
+ */
+
 import Ember from 'ember';
 
 const {
@@ -13,20 +25,47 @@ export default Component.extend({
   attributeBindings: ['style'],
   stepClasses: '',
   classNameBindings: ['stepClasses'],
-  style: computed('index', 'length', 'width', 'height', 'sprite', 'disableInlineStyles', function() {
-    let index = Number(get(this, 'index'));
-    let length = Number(get(this, 'length'));
-    let width = Number(get(this, 'width'));
-    let height = Number(get(this, 'height'));
-    let sprite = get(this, 'sprite');
-    let x = (index * width);
-    let y = 0;
 
-    if (length > 0 && index > length - 1) {
+  defaults: {
+    columns: 1,
+    disableInlineStyles: false,
+    index: 0,
+    maxScale: 1
+  },
+
+  style: computed('index', 'length', 'width', 'height', 'sprite', 'columns', 'disableInlineStyles', function() {
+    const length = Number(get(this, 'length'));
+    const width = Number(get(this, 'width'));
+    const height = Number(get(this, 'height'));
+    const scale = window.devicePixelRatio;
+    let index = Number(get(this, 'index'));
+    let bgSize = (scale > 1) ? width + 'px' : 'auto';
+    let sprite = get(this, 'sprite');
+
+    if (scale > 1) {
+      sprite = sprite.replace(/(\.[0-9a-z]+$)/i, `@${scale}x$1`);
+      bgSize = (width * scale) + 'px';
+    }
+
+    let columns = Number(get(this, 'columns'));
+    let x = -((index % columns) * width);
+    let y = -(Math.floor(index / columns) * height);
+
+    if (length > 0 && index > length) {
       index = length - 1;
     }
 
     set(this, 'stepClasses', 'c-SpriteViewer--slide-' + (index + 1));
-    return (get(this, 'disableInlineStyles') === true) ? null : new Handlebars.SafeString(Handlebars.escapeCSS(`overflow:hidden;width:${width}px;height:${height}px;background:transparent url(${sprite}) no-repeat -${x}px -${y}px;`));
-  })
+    return (get(this, 'disableInlineStyles') === true) ? null : new Handlebars.SafeString(Handlebars.Utils.escapeExpression(`overflow:hidden;width:${width}px;height:${height}px;background:transparent url(${sprite}) no-repeat ${x}px ${y}px;background-size:${bgSize}`));
+  }),
+
+  init() {
+    this._super.apply(this, arguments);
+
+    for (var key in get(this, 'defaults')) {
+      if (!get(this, key)) {
+        set(this, key, get(this, 'defaults')[key]);
+      }
+    }
+  }
 });
